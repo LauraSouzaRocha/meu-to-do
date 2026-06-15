f77c7d3c7a1a958616e60eba677da4a88e7fb13b
[dyad] <lalarocha.rsl@gmail.com>
2026-06-14 22:21:46 -0300

    Updated src/pages/Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -39,7 +39,7 @@ export default function Login() {
         description: "Logged in successfully",
       });
 
-      navigate("/dashboard");
+      navigate("/home");
      } catch (err: any) {
       console.error("LOGIN ERROR:", err);
 
